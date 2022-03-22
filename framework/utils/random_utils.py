422e001468a3abcf273645a39768d995f55a7764
Yury <volodko_ura@mail.ru>
2022-03-22 10:30:36 +0300

    First Commit

---
+++ framework/utils/random_utils.py
@@ -0,0 +1,52 @@
+import random
+import string
+from random import choice
+from framework.constants.random import COUNT_LATTERS_EMAIL, DOMAIN, OTHER, INTERESTS
+
+
+class RandomUtils:
+    email: dict
+
+    @staticmethod
+    def random_email():
+        letters = string.ascii_lowercase
+        your_email = ''.join(
+            choice(letters) for i in range(COUNT_LATTERS_EMAIL))
+        your_domain = choice(DOMAIN)
+        other = choice(OTHER)
+
+        result_email = {
+            'your_email': your_email,
+            'your_domain': your_domain,
+            'other': other,
+        }
+        RandomUtils.email = result_email
+
+        return result_email
+
+    @staticmethod
+    def random_password():
+        letters = string.ascii_uppercase
+        first = choice(letters)
+        second = choice(RandomUtils.email['your_email'])
+        last = ''.join(
+            str(random.randint(0, 9)) for i in range(COUNT_LATTERS_EMAIL))
+
+        result = first + second + last
+        return result
+
+    @staticmethod
+    def random_interests(num):
+        interes_list = INTERESTS
+        result_interests_list = []
+        for i in range(num):
+            val = choice(interes_list)
+            result_interests_list.append(val)
+            interes_list.remove(val)
+        return result_interests_list
+
+    @staticmethod
+    def random_navigate_for_image():
+        x = random.randint(305, 600)
+        y = random.randint(150, 720)
+        return (x, y)
---
+++ tests/test_functional.py
@@ -0,0 +1,46 @@
+from framework.pages.authenticate_page import AuthenticatePage
+from framework.pages.main_page import MainPage
+from framework.pages.profile_page import ProfilePage
+from framework.utils.random_utils import RandomUtils
+from framework.constants.random import COUNT_INTERESTS
+
+class TestFunctional(object):
+
+    def test_authenticate(self, create_browser, base_fix):
+        main_page = MainPage()
+        main_is_open = main_page.is_opened()
+        assert main_is_open, 'Main page is not open'
+        main_page.click_button_here_to_go()
+        auth_page = AuthenticatePage()
+        auth_is_open = auth_page.is_opened()
+        assert auth_is_open, 'Auth page is not open'
+        auth_page.send_password()
+        auth_page.send_email()
+        auth_page.send_domain()
+        auth_page.click_button_other()
+        auth_page.send_other_value()
+        auth_page.click_button_do_not_accept()
+        auth_page.click_button_next()
+
+    def test_help_exit(self, create_browser, base_fix):
+        main_page = MainPage()
+        main_is_open = main_page.is_opened()
+        assert main_is_open, 'Main page is not open'
+        main_page.click_button_here_to_go()
+        auth_page = AuthenticatePage()
+        auth_is_open = auth_page.is_opened()
+        assert auth_is_open, 'Auth page is not open'
+        auth_page.exit_help_window()
+        import time
+        time.sleep(5)
+
+    def test_add_cookies(self, create_browser, base_fix):
+        main_page = MainPage()
+        main_is_open = main_page.is_opened()
+        assert main_is_open, 'Main page is not open'
+        main_page.click_button_here_to_go()
+        auth_page = AuthenticatePage()
+        auth_is_open = auth_page.is_opened()
+        assert auth_is_open, 'Auth page is not open'
+        cookie = auth_page.accept_cookies()
+        assert cookie, 'Cookies не приняты'
